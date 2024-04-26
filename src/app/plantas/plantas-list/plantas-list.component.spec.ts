5bbe20e227b2486c42804da42dc7a942b01cdc61
Orlando Arnedo <Orlando.Arnedo@sagepub.com>
2024-04-26 11:25:43 -0500

    test coverage

--- src/app/plantas/plantas-list/plantas-list.component.spec.ts
+++ src/app/plantas/plantas-list/plantas-list.component.spec.ts
@@ -1,6 +1,39 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PlantasListComponent } from './plantas-list.component';
+import { of } from 'rxjs';
+import { PlantasService } from '../../services/plantas.service';
+
+class MockPlantasService {
+  getAllPlants() {
+    return of([
+      {
+        nombre_comun: 'Rose',
+        nombre_cientifico: 'Rosa rubiginosa',
+        tipo: 'Exterior',
+        altura_maxima: 2,
+        clima: 'Templado',
+        sustrato_siembra: 'Arcilloso',
+      },
+      {
+        nombre_comun: 'Fern',
+        nombre_cientifico: 'Pteridium aquilinum',
+        tipo: 'Interior',
+        altura_maxima: 0.5,
+        clima: 'Húmedo',
+        sustrato_siembra: 'Turba',
+      },
+      {
+        nombre_comun: 'Cactus',
+        nombre_cientifico: 'Carnegiea gigantea',
+        tipo: 'Interior',
+        altura_maxima: 12,
+        clima: 'Seco',
+        sustrato_siembra: 'Arenoso',
+      },
+    ]);
+  }
+}
 
 describe('PlantasListComponent', () => {
   let component: PlantasListComponent;
@@ -8,10 +41,10 @@ describe('PlantasListComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [PlantasListComponent]
-    })
-    .compileComponents();
-    
+      declarations: [PlantasListComponent],
+      providers: [{ provide: PlantasService, useClass: MockPlantasService }],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(PlantasListComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -19,5 +52,12 @@ describe('PlantasListComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should create a table with 3 rows plus the header when 3 plants are loaded', () => {
+    fixture.detectChanges();
+    const rows = fixture.nativeElement.querySelectorAll('tr');
+    const expected = rows.length - 1; //-1 => removing the footer row
+    expect(expected).toEqual(4);
   });
 });
